cf50a48007b3d68ea55ee19201a7bca6dded1c1c
Bradley M. Small <bradley_small@hotamil.com>
2022-12-06 08:26:43 -0500

    After sixth Day Solution

--- Day6/src/main/java/com/small/advent2022/Main.java
+++ Day6/src/main/java/com/small/advent2022/Main.java
@@ -2,9 +2,20 @@ package com.small.advent2022;
 
 import lombok.extern.log4j.Log4j;
 
+import java.io.IOException;
+import java.util.List;
+
+import static com.small.advent2022.AdventSolution.getListFromFile;
+
 @Log4j
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         log.info("Hello world!");
+        TuningTrouble tuningTrouble = new TuningTrouble();
+        List<String> list = getListFromFile("Day6/input.txt");
+        for(String s : list) {
+            log.info("Start Packet :" + tuningTrouble.locateStartPacket(s));
+            log.info("Start Message :" + tuningTrouble.locateStartMessage(s));
+        }
     }
 }
---
+++ Day6/src/main/java/com/small/advent2022/TuningTrouble.java
@@ -0,0 +1,24 @@
+package com.small.advent2022;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TuningTrouble {
+    public int locateStartPacket(String buffer) {
+        return startOfPacket(buffer,  4);
+    }
+
+    private static int startOfPacket(String buffer,  int packetIndicatorLength) {
+        for (int i = 0; i < buffer.length() - (packetIndicatorLength - 1); ++i) {
+            String sub = buffer.substring(i, i + packetIndicatorLength);
+           Set<Integer> set = new HashSet<>(sub.chars().boxed().toList());
+           if(set.size() == packetIndicatorLength)
+               return i + packetIndicatorLength;
+        }
+        return 0;
+    }
+
+    public int locateStartMessage(String buffer) {
+        return startOfPacket(buffer,  14);
+    }
+}
